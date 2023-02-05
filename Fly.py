f40ab9454c0ecefc47aa69798be279ae24ecfb1c
veel <44620496+mariospeterman@users.noreply.github.com>
2023-02-05 20:40:26 +0100

    Create Fly.py

---
+++ Fly.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import ccxt
+from scipy.stats import norm
+
+def broken_wing_butterfly(spot, strike1, strike2, strike3, vol, r, T):
+    d1 = (np.log(spot/strike2) + (r + vol**2/2)*T)/(vol*np.sqrt(T))
+    d2 = (np.log(spot/strike2) + (r - vol**2/2)*T)/(vol*np.sqrt(T))
+    call2 = spot*np.exp(-r*T)*norm.cdf(d1) - strike2*np.exp(-r*T)*norm.cdf(d2)
+    put1 = strike1*np.exp(-r*T) - spot + call2
+    put3 = put1 + strike3 - strike1
+    return call2, put1, put3
+
+def analyze_data(data):
+    # perform technical analysis and return the results
+    return results
+
+# load historical data
+data = pd.read_csv("data.csv")
+
+# analyze the data
+results = analyze_data(data)
+
+# use the results of the analysis to determine the current price and other parameters
+spot = results["current_price"]
+strike1 = results["strike1"]
+strike2 = results["strike2"]
+strike3 = results["strike3"]
+vol = results["implied_volatility"]
+r = results["risk-free_rate"]
+T = results["time_to_expiration"]
+
+# calculate the price of the options
+call2, put1, put3 = broken_wing_butterfly(spot, strike1, strike2, strike3, vol, r, T)
+print("Call option 2:", call2)
+print("Put option 1:", put1)
+print("Put option 3:", put3)
+
+# plot the results for visual analysis
+plt.plot(data["Date"], data["Close"], label="Close Price")
+plt.legend()
+plt.show()
+
+# create an instance of the exchange
+exchange = ccxt.kraken()
+
+# define the order parameters
+symbol = "EUR/USD"
+order_type = "limit"
+side = "buy"
+amount = 1
+price = spot
+
+# add a risk management algorithm
+stop_loss_price = spot - 0.01
+take_profit_price = spot + 0.01
+
+# create and place the order
+order = exchange.create_order(symbol, order_type, side, amount, price)
+exchange.place_order(order)
+
+# add a stop loss and take profit to the order
+stop_loss_order = exchange.create_order(symbol, "stop_loss", side, amount, stop_loss_price)
+take_profit_order = exchange.create_order(symbol, "take_profit", side, amount, take_profit_price)
+
+# Attach stop loss and take profit orders to the original order
+exchange.attach_order(order["id"], stop_loss_order)
+exchange.attach_order(order["id"], take_profit_order)
+
+# Continuously monitor the order status
+while True:
+    order_status = exchange.fetch_order_status(order["id"])
+
+    # Break the loop if the order has been closed
+    if order_status == "closed":
+        break
+    
+    # Re-place the order if it was canceled
+    elif order_status == "canceled":
+        order = exchange.create_order(symbol, type, side, amount, price)
+        exchange.place_order(order)
+
+    # Execute the stop loss or take profit order if triggered
+    elif order_status == "triggered":
+        if order_status == "stop_loss":
+            exchange.place_order(stop_loss_order)
+        elif order_status == "take_profit":
+            exchange.place_order(take_profit_order)
+
+# Log the results
+print("Order executed. Final price:", exchange.fetch_order_price(order["id"]))
